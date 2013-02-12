850ee173d552e272a1f7b36d32fac07a2e7692f4
Harry Jeffery <harry@exec64.co.uk>
2013-02-12 15:16:09 +0000

    Fixed indentation

--- src/init.s
+++ src/init.s
@@ -57,100 +57,101 @@ halt:
 
 .section ".data.pagetable"
 
-	.balign 16384
-	.global kernel_pagetable
+.balign 16384
+.global kernel_pagetable
+
 kernel_pagetable:
-	#Identity map first 256MB of memory (0x00000000-0x0FFFFFFF)
-	# write-back, allocate on write
-	.fill 16, 4, 0x0004140E
-	.fill 16, 4, 0x0104140E
-	.fill 16, 4, 0x0204140E
-	.fill 16, 4, 0x0304140E
-	.fill 16, 4, 0x0404140E
-	.fill 16, 4, 0x0504140E
-	.fill 16, 4, 0x0604140E
-	.fill 16, 4, 0x0704140E
-	.fill 16, 4, 0x0804140E
-	.fill 16, 4, 0x0904140E
-	.fill 16, 4, 0x0A04140E
-	.fill 16, 4, 0x0B04140E
-	.fill 16, 4, 0x0C04140E
-	.fill 16, 4, 0x0D04140E
-	.fill 16, 4, 0x0E04140E
-	.fill 16, 4, 0x0F04140E
+    #Identity map first 256MB of memory (0x00000000-0x0FFFFFFF)
+    # write-back, allocate on write
+    .fill 16, 4, 0x0004140E
+    .fill 16, 4, 0x0104140E
+    .fill 16, 4, 0x0204140E
+    .fill 16, 4, 0x0304140E
+    .fill 16, 4, 0x0404140E
+    .fill 16, 4, 0x0504140E
+    .fill 16, 4, 0x0604140E
+    .fill 16, 4, 0x0704140E
+    .fill 16, 4, 0x0804140E
+    .fill 16, 4, 0x0904140E
+    .fill 16, 4, 0x0A04140E
+    .fill 16, 4, 0x0B04140E
+    .fill 16, 4, 0x0C04140E
+    .fill 16, 4, 0x0D04140E
+    .fill 16, 4, 0x0E04140E
+    .fill 16, 4, 0x0F04140E
 
-        #Identity map second 256MB of memory (0x10000000-0x1FFFFFFF)
-        # write-through, no allocate on write
-	.fill 16, 4, 0x1004040A
-	.fill 16, 4, 0x1104040A
-	.fill 16, 4, 0x1204040A
-	.fill 16, 4, 0x1304040A
-	.fill 16, 4, 0x1404040A
-	.fill 16, 4, 0x1504040A
-	.fill 16, 4, 0x1604040A
-	.fill 16, 4, 0x1704040A
-	.fill 16, 4, 0x1804040A
-	.fill 16, 4, 0x1904040A
-	.fill 16, 4, 0x1A04040A
-	.fill 16, 4, 0x1B04040A
-	.fill 16, 4, 0x1C04040A
-	.fill 16, 4, 0x1D04040A
-	.fill 16, 4, 0x1E04040A
-	.fill 16, 4, 0x1F04040A
+    #Identity map second 256MB of memory (0x10000000-0x1FFFFFFF)
+    # write-through, no allocate on write
+    .fill 16, 4, 0x1004040A
+    .fill 16, 4, 0x1104040A
+    .fill 16, 4, 0x1204040A
+    .fill 16, 4, 0x1304040A
+    .fill 16, 4, 0x1404040A
+    .fill 16, 4, 0x1504040A
+    .fill 16, 4, 0x1604040A
+    .fill 16, 4, 0x1704040A
+    .fill 16, 4, 0x1804040A
+    .fill 16, 4, 0x1904040A
+    .fill 16, 4, 0x1A04040A
+    .fill 16, 4, 0x1B04040A
+    .fill 16, 4, 0x1C04040A
+    .fill 16, 4, 0x1D04040A
+    .fill 16, 4, 0x1E04040A
+    .fill 16, 4, 0x1F04040A
         
-	#Identity map peripherals too (0x20000000-0x20FFFFFF)
-	#Memory = device
-	.fill	16, 4, 0x20040406
+    #Identity map peripherals too (0x20000000-0x20FFFFFF)
+    #Memory = device
+    .fill 16, 4, 0x20040406
 
-	#Unmapped (0x21000000-0xBFFFFFFF)
-	.fill   4*2544, 1, 0
+    #Unmapped (0x21000000-0xBFFFFFFF)
+    .fill 4*2544, 1, 0
 
-	#Linear map first 256MB of memory (0xC0000000-0xCFFFFFFF)
-	# write-back, allocate on write
-	.fill 16, 4, 0x0004140E
-	.fill 16, 4, 0x0104140E
-	.fill 16, 4, 0x0204140E
-	.fill 16, 4, 0x0304140E
-	.fill 16, 4, 0x0404140E
-	.fill 16, 4, 0x0504140E
-	.fill 16, 4, 0x0604140E
-	.fill 16, 4, 0x0704140E
-	.fill 16, 4, 0x0804140E
-	.fill 16, 4, 0x0904140E
-	.fill 16, 4, 0x0A04140E
-	.fill 16, 4, 0x0B04140E
-	.fill 16, 4, 0x0C04140E
-	.fill 16, 4, 0x0D04140E
-	.fill 16, 4, 0x0E04140E
-	.fill 16, 4, 0x0F04140E
+    #Linear map first 256MB of memory (0xC0000000-0xCFFFFFFF)
+    # write-back, allocate on write
+    .fill 16, 4, 0x0004140E
+    .fill 16, 4, 0x0104140E
+    .fill 16, 4, 0x0204140E
+    .fill 16, 4, 0x0304140E
+    .fill 16, 4, 0x0404140E
+    .fill 16, 4, 0x0504140E
+    .fill 16, 4, 0x0604140E
+    .fill 16, 4, 0x0704140E
+    .fill 16, 4, 0x0804140E
+    .fill 16, 4, 0x0904140E
+    .fill 16, 4, 0x0A04140E
+    .fill 16, 4, 0x0B04140E
+    .fill 16, 4, 0x0C04140E
+    .fill 16, 4, 0x0D04140E
+    .fill 16, 4, 0x0E04140E
+    .fill 16, 4, 0x0F04140E
 
-        #Linear map second 256MB of memory (0xD0000000-0xDFFFFFFF)
-        # write-through, no allocate on write
-	.fill 16, 4, 0x1004040A
-	.fill 16, 4, 0x1104040A
-	.fill 16, 4, 0x1204040A
-	.fill 16, 4, 0x1304040A
-	.fill 16, 4, 0x1404040A
-	.fill 16, 4, 0x1504040A
-	.fill 16, 4, 0x1604040A
-	.fill 16, 4, 0x1704040A
-	.fill 16, 4, 0x1804040A
-	.fill 16, 4, 0x1904040A
-	.fill 16, 4, 0x1A04040A
-	.fill 16, 4, 0x1B04040A
-	.fill 16, 4, 0x1C04040A
-	.fill 16, 4, 0x1D04040A
-	.fill 16, 4, 0x1E04040A
-	.fill 16, 4, 0x1F04040A
+    #Linear map second 256MB of memory (0xD0000000-0xDFFFFFFF)
+    # write-through, no allocate on write
+    .fill 16, 4, 0x1004040A
+    .fill 16, 4, 0x1104040A
+    .fill 16, 4, 0x1204040A
+    .fill 16, 4, 0x1304040A
+    .fill 16, 4, 0x1404040A
+    .fill 16, 4, 0x1504040A
+    .fill 16, 4, 0x1604040A
+    .fill 16, 4, 0x1704040A
+    .fill 16, 4, 0x1804040A
+    .fill 16, 4, 0x1904040A
+    .fill 16, 4, 0x1A04040A
+    .fill 16, 4, 0x1B04040A
+    .fill 16, 4, 0x1C04040A
+    .fill 16, 4, 0x1D04040A
+    .fill 16, 4, 0x1E04040A
+    .fill 16, 4, 0x1F04040A
 
-	#Unmapped (0xE0000000-0xF1FFFFFF)
-	.fill   4*288, 1, 0
+    #Unmapped (0xE0000000-0xF1FFFFFF)
+    .fill   4*288, 1, 0
         
-	#Linear map peripherals (0xF2000000-0xF2FFFFFF)
-	#Using same layout as linux, as given by
-	#BCM2825-ARM-Peripherals manual
-	#Memory = device
-	.fill	16, 4, 0x20040406
+    #Linear map peripherals (0xF2000000-0xF2FFFFFF)
+    #Using same layout as linux, as given by
+    #BCM2825-ARM-Peripherals manual
+    #Memory = device
+    .fill	16, 4, 0x20040406
 
-	#Unmapped (0xF3000000-0xFFFFFFFF)
-	.fill   4*208, 1, 0
+    #Unmapped (0xF3000000-0xFFFFFFFF)
+    .fill   4*208, 1, 0
