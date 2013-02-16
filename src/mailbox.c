2132f445150189d08aadc553828b6bd4db7c5142
Harry Jeffery <harry@exec64.co.uk>
2013-02-16 19:27:15 +0000

    Tweaked mailbox code

--- src/mailbox.c
+++ src/mailbox.c
@@ -5,28 +5,30 @@
 #define MAILBOX_DATA_MASK 0xFFFFFFF0
 #define MAILBOX_CHAN_MASK 0x0000000F
 
+uint32_t mailbox_buffer[256] __attribute__((aligned (16)));
+
 void mailbox_send(uint32_t data, uint32_t channel) {
-    while(0 != (mmio_read(MAILBOX_STATUS) & (1 << 31))) { ; }
+    while(0 != (mmio_read(MAILBOX_0_STATUS) & (1 << 31))) { ; }
 
     uint32_t message = 0;
-    message |= (data << 4) & MAILBOX_DATA_MASK;
+    message |= data & MAILBOX_DATA_MASK;
     message |= channel & MAILBOX_CHAN_MASK;
 
-    mmio_write(MAILBOX_WRITE, message);
+    mmio_write(MAILBOX_0_WRITE, message);
 }
 
 uint32_t mailbox_read(uint32_t channel) {
     channel &= MAILBOX_CHAN_MASK;
 
     while(TRUE) {
-        while(0 != (mmio_read(MAILBOX_STATUS) & (1 << 30))) { ; }
+        while(0 != (mmio_read(MAILBOX_0_STATUS) & (1 << 30))) { ; }
 
-        uint32_t message = mmio_read(MAILBOX_READ);
+        uint32_t message = mmio_read(MAILBOX_0_READ);
         uint32_t data = message & MAILBOX_DATA_MASK;
         uint32_t chan = message & MAILBOX_CHAN_MASK;
 
         if(chan == channel) {
-            return data >> 4;
+            return data;
         }
     }
 }
