df96d73e74af9ef8f7cb299c7fd727334946ea06
Harry Jeffery <harry@exec64.co.uk>
2013-12-05 22:38:50 +0000

    Added support for prefetch/data abort interrupts

--- src/interrupts.c
+++ src/interrupts.c
@@ -4,7 +4,7 @@
 #include "uart.h"
 
 
-__attribute__ ((naked)) void int_halt() { while(1); }
+__attribute__ ((naked)) void halt() { while(1); }
 
 __attribute__ ((interrupt ("SWI"))) void int_swi() {
     uint32_t next_ins, swi;
@@ -15,15 +15,32 @@ __attribute__ ((interrupt ("SWI"))) void int_swi() {
     uart_printf("Received SWI %i returns to 0x%x\n", swi, next_ins);
 }
 
+__attribute__ ((interrupt ("ABORT"))) void int_prefetch_abort() {
+    uart_printf("hit prefetch abort\n");
+    halt();
+}
+
+__attribute__ ((interrupt ("ABORT"))) void int_data_abort() {
+    uint32_t fault_addr, fault_status;
+    __asm volatile("mrc p15, 0, %0, c6, c0, 0" : "=r" (fault_addr) );
+    __asm volatile("mrc p15, 0, %0, c5, c0, 0" : "=r" (fault_status) );
+
+
+    uart_puts("MMU DATA ABORT\n");
+    uart_printf("fault address: 0x%x\n", fault_addr);
+    uart_printf("fault status: 0b%b\n", fault_status);
+    halt();
+}
+
 __attribute__ ((naked, aligned(32))) static void vectors(void) {
-    __asm volatile("b int_halt;"
-                   "b int_halt;"
+    __asm volatile("b halt;"
+                   "b halt;"
                    "b int_swi;"
-                   "b int_halt;"
-                   "b int_halt;"
-                   "b int_halt;"
-                   "b int_halt;"
-                   "b int_halt;"
+                   "b int_prefetch_abort;"
+                   "b int_data_abort;"
+                   "b halt;"
+                   "b halt;"
+                   "b halt;"
                    );
 }
 
